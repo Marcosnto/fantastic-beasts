3a6152a25977a005146d4a7a460f5cd366a71c2e
Marcos Neto <marcos_nto@hotmail.com>
2020-11-22 02:02:58 -0300

    Exercise 3 - Class - 0306 - Correction

--- script.js
+++ script.js
@@ -2,25 +2,43 @@
 // em relação ao topo da página
 const firstImage = document.querySelector('img');
 console.log(firstImage.getBoundingClientRect().top);
+//poderia ser também:
+console.log(firstImage.offsetTop);
+
 // Retorne a soma da largura de todas as imagens
-const allImages = document.querySelectorAll('img');
+/** Para resolver essa questão é necessário esperar
+ * o navegador carregar a página por completo, pois o JS é executado
+ * antes do carregamento, sendo assim, como as larguras das imagens
+ * não estão pré-definidas, não é possível obter tal propriedade.
+ */
 
-let widthSomatory = 0;
-allImages.forEach((img) => {
-  widthSomatory += img.getBoundingClientRect().y
-})
+function somaImagens(){
+  const allImages = document.querySelectorAll('img');
+  
+  let widthSomatory = 0;
+  allImages.forEach((img) => {
+    widthSomatory += img.offsetWidth;
+  })
+  
+  console.log(widthSomatory);
+}
 
-console.log(widthSomatory);
+window.onload = function() {
+  somaImagens();
+}
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
+
+// Verifique se os links da página possuem o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 const allLinks = document.querySelectorAll('a');
 allLinks.forEach((link) => {
-  let linkAttributes = link.getBoundingClientRect();
-  let isSmall = linkAttributes.Y === 48 && linkAttributes.X === 48 ? 'possui o padrão' : 'não possui o padrão'; 
+  let linkAttributesY = link.offsetHeight;
+  let linkAttributesX = link.offsetWidth;
+  let isSmall = linkAttributesY >= 48 && linkAttributesX >= 48 ? 'possui o padrão' : 'não possui o padrão'; 
   console.log(`O link: ${link} ${isSmall}`)
 })
+
+
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
 const getWindowSize = window.innerWidth; 
